97563e6336c0d56b3e8bc8c42d36ba7e581e9262
atshaw1994 <atshaw1994@gmail.com>
2025-11-13 07:01:53 -0500

    Fix signup link and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+	before_action do
+		Rails.logger.info "Session: \\#{session.to_hash}"
+		Rails.logger.info "Cookies: \\#{cookies.to_hash}"
+	end
 end
